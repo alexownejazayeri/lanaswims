05d053e684c4d867ce796b22ecda3a04d05a05b5
Alex Ownejazayeri <alirezaownejazayeri@gmail.com>
2024-12-24 20:52:14 -0800

    removes vite icon to test netlify

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "./App.css";
 
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "./vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,9 +9,9 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        {/* <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        </a> */}
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
